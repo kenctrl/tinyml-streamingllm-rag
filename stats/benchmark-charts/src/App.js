ecc65849327aab245f0a63e55a5184cd54d892c4
kenctrl <kennethkchoi22@gmail.com>
2024-12-04 14:42:18 -0500

    npm lol

---
+++ stats/benchmark-charts/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BenchmarkCharts from './components/BenchmarkCharts';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Benchmark Results</h1>
+      <BenchmarkCharts />
+    </div>
+  );
+}
+
+export default App;
